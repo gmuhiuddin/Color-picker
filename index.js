7f08cbfcc572a25cd26494406491e96e2e7cba73
Ghulam muhiuddin <muhiuddinghulam825@gmail.com>
2024-04-13 00:23:44 +0500

    add some changessss

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 let startingColor = document.getElementById("color-starting");
 let endingColor = document.getElementById("color-ending");
-
 let previewDiv = document.getElementById("preview");
 let body = document.getElementsByTagName('body');
+let checkBtn = document.getElementById("gen-gradient-btn");
+
 function preview (){
 previewDiv.style.background = `linear-gradient(to right ,${startingColor.value},${endingColor.value})`
-}
+};
+
 setInterval(()=>{
     preview()
-},100)
+},100);
 
-let checkBtn = document.getElementById("gen-gradient-btn");
-checkBtn.addEventListener('click',backgroundChange)
+checkBtn.addEventListener('click',backgroundChange);
 
 function backgroundChange (){
     body[0].style.background = `linear-gradient(to right ,${startingColor.value},${endingColor.value})`
-}
+};
